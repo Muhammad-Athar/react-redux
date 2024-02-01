2f800f738172337186d5db1a1d107a302643cbde
Muhammad Athar <58026305+Muhammad-Athar@users.noreply.github.com>
2024-02-01 15:56:26 +0500

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../ui/Button";
+
 function PageNotFound() {
     return (
         <div className="text-xl space-x-3">
             <span className="font-semibold text-red-600">ERROR 404:</span>
             <span>page not found :(</span>
+            <Button onClick={() => navigate("/")}>Go Back<Botton/>
         </div>
     );
 }
